fcbf8b280f28758f6591c8ff7dfc8397469e4794
asb19 <amirsohel.as10@gmail.com>
2025-09-03 07:10:29 +0000

    feat: added migrations

---
+++ tasksvc/migrations/001_init.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title TEXT NOT NULL,
+    description TEXT,
+    status TEXT NOT NULL CHECK (status IN ('pending', 'inprogress', 'done')),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
